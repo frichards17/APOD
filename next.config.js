d308154735802bd20ac3f014f487c20e81dc9359
Frank Richards <56968405+frichards17@users.noreply.github.com>
2023-12-15 10:25:32 +0000

    Replaced image domains with remotePatterns and added sharp package

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['apod.nasa.gov'],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'apod.nasa.gov',
+          pathname: '**',
+        },
+      ],
       },
 }
 
